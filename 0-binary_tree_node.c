39ed72a4a8f0ca42a9a656e1253cb901a79077b6
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-26 08:50:51 +0100

    betty style debugging

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-       
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new->right = NULL;
 	new->parent = parent;
 	new->n = value;
-	return(new);	
+	return (new);
 }
